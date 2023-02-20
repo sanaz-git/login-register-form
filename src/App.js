eea36c1c46836e99ac6cb84ff2480fa3bdfeebc9
Sanaz <szeraati69@gmail.com>
2023-02-20 23:40:58 +0330

    create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
 import './App.css';
+import BackBox from './components/BackBox';
+import Login from './components/Login';
+import Register from './components/Register';
+
 
 function App() {
   return (
-    <div className="App">
-      app
+  
+    <div className="flex w-full h-screen items-center justify-center">
+      <div > <BackBox/></div>
+      <div><Login/></div>
+        <div><Register/></div>
+      
+     
+     
+      
     </div>
   );
 }
---
+++ src/components/BackBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const BackBox = () => {
+    return (
+      
+        <div   className='hidden lg:flex w-[40rem] h-60 bg-[#404040] items-center justify-center text-white'>
+            BackBox      
+        </div>
+    );
+};
+
+export default BackBox; 
